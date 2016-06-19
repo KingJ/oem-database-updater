133628c7453434a655d96b74044484ca3d112eee
Dean Gardiner <me@dgardiner.net>
2016-06-20 01:28:00 +1200

    Updated [setup.py] to use pbr

--- setup.py
+++ setup.py
@@ -2,17 +2,6 @@ from setuptools import setup
 
 
 setup(
-    name='oem-updater',
-    version='1.0.0',
-
-    author="Dean Gardiner",
-    author_email="me@dgardiner.net",
-
-    install_requires=[
-        'appdirs>=1.4.0',
-        'msgpack-python>=0.4.7',
-
-        'git+git://github.com/fuzeman/anidb.py.git#egg=anidb.py',
-        'git+git://github.com/fuzeman/tvdb_api.git#egg=tvdb_api'
-    ]
+    setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
+    pbr=True,
 )
